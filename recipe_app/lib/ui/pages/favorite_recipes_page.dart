b4126852aeb770edfd84cea2c08e4a8c7a815ce1
Ali Alperen Derici <42701925+alperenderici@users.noreply.github.com>
2023-03-29 18:33:24 +0300

    Setup project infrastructure

---
+++ recipe_app/lib/ui/pages/favorite_recipes_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class FavoriteRecipesPage extends StatefulWidget {
+  const FavoriteRecipesPage({super.key});
+
+  @override
+  State<FavoriteRecipesPage> createState() => _FavoriteRecipesPageState();
+}
+
+class _FavoriteRecipesPageState extends State<FavoriteRecipesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Favorite Recipes Page'),
+      ),
+    );
+  }
+}
